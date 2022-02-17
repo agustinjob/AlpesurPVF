293b1537ebfacd5f4579fdea5c0be3346a8ac74c
Agustín Job Hernández Montes <agus_job@hotmail.com>
2022-02-17 10:07:39 -0600

    bd correcta

--- src/main/java/bd/puntoalpesur.sql
+++ src/main/java/bd/puntoalpesur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-01-2022 a las 04:40:51
+-- Tiempo de generación: 17-02-2022 a las 17:03:36
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.4.15
 
